0bbcbaf6a75bf73bddbd65f8a9482be81ce3dd09
Codushan <118370072+Codushan@users.noreply.github.com>
2025-03-01 02:31:40 +0530

    Update and rename eslint.config.mjs to eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import nextjs from 'eslint-config-next';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+
+export default [
+  ...nextjs,
+  {
+    plugins: {
+      'react': react,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
+];
